6caa37d1cc0e841d141ab21e9181cde828382813
José Eduardo <limaedu18@hotmail.com>
2025-04-24 19:46:31 -0300

    First commit

---
+++ src/main/java/j_edu_lima/metastic/MetasticApplication.java
@@ -0,0 +1,13 @@
+package j_edu_lima.metastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/j_edu_lima/metastic/MetasticApplicationTests.java
@@ -0,0 +1,13 @@
+package j_edu_lima.metastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
